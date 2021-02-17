1e19945226cb3676809e83187d19a77ff4360b5a
Eliel Laynes <ElielLaynes@users.noreply.github.com>
2021-02-17 14:07:42 -0300

    Lab 03 - jogo da forca

--- Cap05/Lab03/forca_v1.py
+++ Cap05/Lab03/forca_v1.py
@@ -1,13 +1,13 @@
-# Hangman Game (Jogo da Forca) 
+# Hangman Game (Jogo da Forca)
 # Programação Orientada a Objetos
 
 # Import
 import random
 
-# Board (tabuleiro)
-board = ['''
+# Board (tabuleiro) -> "corpo do jogo", é onde vai aparecer a forca se formando
+corpo = ['''
 
->>>>>>>>>>Hangman<<<<<<<<<<
+>>>>>>>>>>Jogo da Forca<<<<<<<<<<
 
 +---+
 |   |
@@ -67,60 +67,90 @@ O   |
 
 
 # Classe
-class Hangman:
+class enforcado:
 
 	# Método Construtor
-	def __init__(self, word):
-		
+	def __init__(self, palavra):
+		self.palavra = palavra
+		self.letra_errada = []
+		self.letra_certa = []
+		print('classe inicidada')
 		
 	# Método para adivinhar a letra
-	def guess(self, letter):
-		
+	def adivinhar(self, letra):
+		if letra in self.palavra and letra not in self.letra_certa:
+			self.letra_certa.append(letra)
+		elif letra not in self.palavra and letra not in self.letra_errada:
+			self.letra_errada.append(letra)
+		else:
+			return False
+		return True
 		
 	# Método para verificar se o jogo terminou
-	def hangman_over(self):
-		
+	def jogo_terminou(self):
+		return self.jogador_venceu() or (len(self.letra_errada) == 6)
 		
 	# Método para verificar se o jogador venceu
-	def hangman_won(self):
-		
+	def jogador_venceu(self):
+		if '_' not in self.esconder_palavra():
+			return True
+		return False
 
-	# Método para não mostrar a letra no board
-	def hide_word(self):
-		
+	# Método para não mostrar a letra da palavra correta no board
+	def esconder_palavra(self):
+		lc = ''
+		for letra in self.palavra:
+			if letra not in self.letra_certa:
+				lc += ' '
+			else:
+				lc += letra
+		return lc
 		
 	# Método para checar o status do game e imprimir o board na tela
-	def print_game_status(self):
-		
+	def print_status_jogo(self):
+		print(corpo[len(self.letra_errada)])
+		print('\nPalavra: ' + self.esconder_palavra())
+		print('\nLetras Erradas: ',)
+		for letra in self.letra_errada:
+			print(letra)
+		print('\nLetras corretas: ',)
+		for letra in self.letra_certa:
+			print(letra)
+		print()
 
 # Função para ler uma palavra de forma aleatória do banco de palavras
-def rand_word():
-        with open("palavras.txt", "rt") as f:
-                bank = f.readlines()
-        return bank[random.randint(0,len(bank))].strip()
+def palavra_aleatoria():
+        with open("palavras.txt", "rt") as pa:
+                banco_palavras = pa.readlines()
+        return banco_palavras[random.randint(0,len(banco_palavras))].strip()
 
 
-# Função Main - Execução do Programa
+# Função Main - Execução do Programa >>>PROGRAMA PRINCIPAL<<<<
 def main():
 
 	# Objeto
-	game = Hangman(rand_word())
+	jogo = enforcado(palavra_aleatoria())
 
 	# Enquanto o jogo não tiver terminado, print do status, solicita uma letra e faz a leitura do caracter
-	
+	while not jogo.jogo_terminou():
+		jogo.print_status_jogo()
+		usuario_input = input('Digirte uma Letra: ')
+		jogo.adivinhar(usuario_input)
 
 	# Verifica o status do jogo
-	game.print_game_status()	
+	jogo.print_status_jogo()
 
 	# De acordo com o status, imprime mensagem na tela para o usuário
-	if game.hangman_won():
-		print ('\nParabéns! Você venceu!!')
+	if jogo.jogador_venceu():
+		print('\nParabéns! Você venceu!!')
 	else:
-		print ('\nGame over! Você perdeu.')
-		print ('A palavra era ' + game.word)
-		
-	print ('\nFoi bom jogar com você! Agora vá estudar!\n')
+		print('\nGame over! Você perdeu.')
+		print('A palavra era ' + jogo.palavra)
 
-# Executa o programa		
+	print('\nFoi bom jogar com você! Agora vá estudar!\n')
+
+# Executa o programa
 if __name__ == "__main__":
 	main()
+
+	
